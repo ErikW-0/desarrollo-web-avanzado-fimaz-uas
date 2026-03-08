4b39e7201a82b5e48ddee2a35a97ad35c9e730a8
ErikW-O <erikwr3@gmail.com>
2026-03-08 16:51:11 -0700

    Crear objeto Admin y mostrar nombre, correo y rol

---
+++ parcial-1-poo/practica-2/index.php
@@ -0,0 +1,11 @@
+<?php
+include ("Admin.php");
+include ("Usuario.php");
+
+$objadmin1 = new Admin("erikwr3@gmail,com", "Erik Watson");
+ echo "Nombre:" . $objadmin1->getNombre();
+  echo "Correo:" . $objadmin1->getCorreo();
+   echo "Rol:" . $objadmin1->getRol();
+
+?>
+ 
