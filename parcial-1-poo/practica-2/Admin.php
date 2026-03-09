184069d4cb665b90e82bd8049f530c0ae0cebe73
ErikW-O <erikwr3@gmail.com>
2026-03-08 18:09:04 -0700

    salto de linea agregado

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,5 +1,5 @@
-/* The Admin class extends the Usuario class and defines a method to return the role as
-"administrador". */
+
+
 <?php
 require_once 'Usuario.php';
 class Admin extends Usuario{
--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -3,8 +3,8 @@ require_once 'Admin.php';
 require_once 'Usuario.php';
 
 $objadmin1 = new Admin("erikwr3@gmail,com", "Erik Watson");
- echo "Nombre:" . $objadmin1->getNombre();
-  echo "Correo:" . $objadmin1->getCorreo();
+ echo "Nombre:" . $objadmin1->getNombre() . "<br>";
+  echo "Correo:" . $objadmin1->getCorreo() . "<br>";
    echo "Rol:" . $objadmin1->getRol();
 
 ?>
