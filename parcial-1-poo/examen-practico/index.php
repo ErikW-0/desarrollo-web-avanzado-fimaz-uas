e812e84b2f0a6448e5cf47443495b5262881cfd2
ErikW-O <erikwr3@gmail.com>
2026-03-11 16:49:57 -0700

    creacion del index con la tabla de usuarios

--- parcial-1-poo/examen-practico/index.php
+++ parcial-1-poo/examen-practico/index.php
@@ -4,4 +4,36 @@ include_once("Usuario.php");
 include_once("Admin.php");
 include_once("Alumno.php");
 
+$objusuarios = [];
+try {
+    $objusuarios [] = new Admin("Erik Watson", "erikwr3@gmail.com");
+    $objusuarios [] = new Alumno("Pedro", "pedro@gmail.com", "12345");
+    $objusuarios [] = new Alumno("Juan Error", "correo-malo", "54321");
 
+}catch (Exception $e) {
+    echo "<p style='color:red;'>se detecto un problema: ".$e->getMessage()."</p>";
+}
+?>
+<h2>Lista de Usuarios</h2>
+
+<table border="1" cellpadding="8">
+<tr>
+<th>Nombre</th>
+<th>Correo</th>
+<th>Rol</th>
+<th>Matrícula</th>
+</tr>
+
+<?php
+foreach($objusuarios as $u){
+
+    $matricula = method_exists($u, "getMatricula") ? $u->getMatricula() : "—";
+    
+    echo "<tr>";
+    echo "<td>".$u->getNombre()."</td>";
+    echo "<td>".$u->getCorreo()."</td>";
+    echo "<td>".$u->getRol()."</td>";
+    echo "<td>".$matricula."</td>";
+    echo "</tr>";
+
+}
