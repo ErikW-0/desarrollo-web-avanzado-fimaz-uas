a7f74f38e0b5de1edf572410437b8256fa942b73
ErikW-O <erikwr3@gmail.com>
2026-03-11 16:17:17 -0700

    se creo la clase usuario con su varible de matricula y su rol

---
+++ parcial-1-poo/examen-practico/Alumno.php
@@ -0,0 +1,18 @@
+<?php
+include_once "Usuario.php";
+
+class Aumno extends Usuario{
+    private $vMatricula;
+    public function __construct($vNombre, $vCorreo, $vMatricula){
+        parent::__construct($vNombre, $vCorreo);
+
+        $this->vMatricula = $vMatricula;
+    }
+    public function getMatricula(){
+        return $this->vMatricula;
+    }
+    public function getRol(){
+        return "alumno";
+    }
+
+}
